b4e861de68f27a2d410addcc74dc3fd2cd563bbd
Jocelyn Chicaiza <Xavier@DESKTOP-NJ0503B>
2020-07-03 18:38:52 -0500

    Lista circular

---
+++ Proyecto/src/TDA/LCDE.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TDA;
+
+/**
+ *
+ * @author Jocelyn Chicaiza
+ */
+public class LCDE {
+    int size=0;
+    NodeList inicio;
+    NodeList fin;
+    
+}
