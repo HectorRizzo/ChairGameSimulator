29674396c393e7fafb992e60b17030809726f147
Jocelyn Chicaiza <Xavier@DESKTOP-NJ0503B>
2020-07-10 17:24:32 -0500

    pos X ,Y

--- Proyecto/src/principal/usuario.java
+++ Proyecto/src/principal/usuario.java
@@ -5,8 +5,11 @@ import javafx.scene.image.ImageView;
 
 
 public class usuario {
+   
     private ImageView image;
     private boolean seated;
+    private double posX;
+    private double posY;
 
     public usuario() {
         image = new ImageView("usuario.jpg");
@@ -22,6 +25,22 @@ public class usuario {
 
     public void setIamgen(ImageView image) {
         this.image = image;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
     }
     
     
